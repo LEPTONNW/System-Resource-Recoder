fc820b59939d658dc1edafa3c420687df1bb0545
Round Robin <kac4484@gmail.com>
2021-05-20 23:12:25 +0900

    Add files via upload

---
+++ System Resource Recoder/SingUP.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace System_Resource_Recoder
+{
+    partial class SingUP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SingUP));
+            this.Info = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Info
+            // 
+            this.Info.AutoSize = true;
+            this.Info.Font = new System.Drawing.Font("한컴 바겐세일 M", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Info.ForeColor = System.Drawing.Color.SkyBlue;
+            this.Info.Location = new System.Drawing.Point(10, 128);
+            this.Info.Name = "Info";
+            this.Info.Size = new System.Drawing.Size(306, 22);
+            this.Info.TabIndex = 20;
+            this.Info.Text = "이메일과 패스워드는 신중하게 입력해 주세요.";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "@gmail.com",
+            "@naver.com",
+            "@nate.com",
+            "@daum.net",
+            "@hanmail.net",
+            "@korea.com",
+            "@yahoo.com",
+            "@hotmail.com",
+            "@chol.com",
+            "@netian.com",
+            "@dreamwiz.com",
+            "@live.co",
+            "직접입력"});
+            this.comboBox1.Location = new System.Drawing.Point(187, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 20);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(166)))), ((int)(((byte)(205)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("한컴 바겐세일 M", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(37, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 73);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "메 일 등 록";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(99, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 21);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(81, 21);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("한컴 바겐세일 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(26, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 21);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "패스워드:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("한컴 바겐세일 M", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(33, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 21);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "이메일 :";
+            // 
+            // SingUP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(327, 280);
+            this.Controls.Add(this.Info);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SingUP";
+            this.Text = "시작프그램 및 메일보고등록";
+            this.Load += new System.EventHandler(this.SingUP_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Info;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ System Resource Recoder/SingUP.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace System_Resource_Recoder
+{
+    public partial class SingUP : Form
+    {
+        public SingUP()
+        {
+            InitializeComponent();
+        }
+
+        private void SingUP_Load(object sender, EventArgs e)
+        {
+            textBox2.Hide();
+            label2.Hide();
+
+            comboBox1.Text = "직접입력";
+        }
+
+        public string FilePath = "C:\\Windows\\SRR.ini";
+        public string Email = "";
+        
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    MessageBox.Show("정보가 입력되지 않았습니다.");
+                    return;
+                }
+                else if(string.IsNullOrEmpty(textBox2.Text))
+                {
+                    textBox2.Text = "";
+                }
+
+
+
+                Email = textBox1.Text + comboBox1.Text;
+                CreateINI();
+
+                if (!File.Exists("C:\\Windows\\SRR_Key.ocx"))
+                {
+                    RegistryKey rkey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    rkey.SetValue("ProgramName", Application.ExecutablePath.ToString());
+                    MessageBox.Show("시작프로그램에 자동등록되었습니다.");
+                    File.Create("C:\\Windows\\SRR_Key.ocx");
+                }
+                else
+                {
+                    MessageBox.Show("메일은 최대 1개 까지만 등록이 가능합니다." + Environment.NewLine + "메일을 새로 등록하려면 삭제후 다시 만들어 주세요");
+                }
+
+            }
+            catch
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                comboBox1.Text = "직접입력";
+                File.Delete("C:\\Windows\\SRR.ini");
+
+                if (File.Exists("C:\\Windows\\SRR_Key.ocx"))
+                {
+                    File.Delete("C:\\Windows\\SRR_Key.ocx");
+                }
+                else
+
+                    MessageBox.Show("정상적으로 입력되지 않았습니다.");
+            }
+
+            MessageBox.Show("메일 등록이 완료되었습니다.");
+            this.Close();
+        }
+
+        void CreateINI() //유저 정보파일 생성
+        {
+            WritePrivateProfileString("섹션1", "ID", Email, FilePath);
+            WritePrivateProfileString("섹션1", "PASS", textBox2.Text, FilePath);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
